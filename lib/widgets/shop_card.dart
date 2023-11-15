8784afb40a8841eca0667519f24d3a0257b586fc
shantiyoga <shantiyoga@ui.ac.id>
2023-11-15 14:59:25 +0700

    Tutorial 8

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/screens/login.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/list_product.dart';
-
-
+// Impor drawer widget
 
 class ShopItem {
   final String name;
@@ -21,24 +20,26 @@ class ShopCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      final request = context.watch<CookieRequest>();
-      return Material(
+    final request = context.watch<CookieRequest>();
+    return Material(
       color: Colors.indigo,
       child: InkWell(
         // Area responsive terhadap sentuhan
-        // Area responsif terhadap sentuhan
         onTap: () async {
           // Memunculkan SnackBar ketika diklik
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
-
           // Navigate ke route yang sesuai (tergantung jenis tombol)
           if (item.name == "Tambah Produk") {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ShopFormPage()));
-          }
+            // TODO: Gunakan Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup ShopFormPage.
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ShopFormPage()));
+          } else if (item.name == "Lihat Produk") {
+            Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ProductPage()));
+          } // tambahkan else if baru seperti di bawah ini
           else if (item.name == "Logout") {
                   final response = await request.logout(
                       // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
@@ -59,7 +60,8 @@ class ShopCard extends StatelessWidget {
                     ));
                   }
                 }
-      },
+
+        },
         child: Container(
           // Container untuk menyimpan Icon dan Text
           padding: const EdgeInsets.all(8),
